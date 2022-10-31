5863cf5cbe117988df434369388b45625315f988
blesstosam <805902285@qq.com>
2022-10-31 15:46:09 +0800

    feat: 适配qiankun

--- packages/web/src/main.js
+++ packages/web/src/main.js
@@ -1,11 +1,53 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createWebHistory, createRouter } from 'vue-router'
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
+import App from './App.vue'
 import { registerCom } from './components/registerCom'
+import { routes } from './router'
 
-const app = createApp(App)
-app.use(router).use(ElementPlus).mount('#app')
+import { renderWithQiankun, qiankunWindow } from 'vite-plugin-qiankun/dist/helper'
 
-registerCom(app)
+let router = null
+let app = null
+let history = null
+
+function render(props = {}) {
+  const { container } = props
+  history = createWebHistory(qiankunWindow.__POWERED_BY_QIANKUN__ ? '/designer' : '/')
+  router = createRouter({
+    history,
+    routes
+  })
+
+  app = createApp(App)
+  app
+    .use(router)
+    .use(ElementPlus)
+    .mount(container ? container.querySelector('#app') : '#app')
+  registerCom(app)
+}
+
+renderWithQiankun({
+  bootstrap() {
+    console.log('designer bootstrap')
+  },
+  mount(props) {
+    console.log('designer mount')
+    render(props)
+    app.config.globalProperties.$onGlobalStateChange = props.onGlobalStateChange
+    app.config.globalProperties.$setGlobalState = props.setGlobalState
+  },
+  unmount(props) {
+    console.log('designer unmount')
+    app.unmount()
+    app._container.innerHTML = ''
+    app = null
+    router = null
+    history.destroy()
+  }
+})
+
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+  render({})
+}
